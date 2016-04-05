836c01ff31193302d7bfcc22e7206041f3e848b2
dsick <sick@davesick.net>
2016-04-05 10:49:32 -0700

    assignment 30 complete

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,27 +7,39 @@
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 require 'random_data'
 
- # Create Posts
- 50.times do
- # #1
-   Post.create!(
- # #2
-     title:  RandomData.random_sentence,
-     body:   RandomData.random_paragraph
-   )
- end
- posts = Post.all
- 
- # Create Comments
- # #3
- 100.times do
-   Comment.create!(
- # #4
-     post: posts.sample,
-     body: RandomData.random_paragraph
-   )
- end
- 
- puts "Seed finished"
- puts "#{Post.count} posts created"
- puts "#{Comment.count} comments created"
+# Find or Create unique post
+Post.find_or_create_by(title: 'My unique post title') do |post| post.body = 'My unique post body'
+end
+
+Comment.find_or_create_by(body: 'a uniue comment body 1') do |comment|
+  comment.post_id = 1
+end
+
+Comment.find_or_create_by(body: 'a uniue comment body 2') do |comment|
+  comment.post_id = 1
+end
+
+# Create Posts
+50.times do
+  # #1
+  Post.create!(
+    # #2
+    title:  RandomData.random_sentence,
+    body:   RandomData.random_paragraph
+  )
+end
+posts = Post.all
+
+# Create Comments
+# #3
+100.times do
+  Comment.create!(
+    # #4
+    post: posts.sample,
+    body: RandomData.random_paragraph
+  )
+end
+
+puts "Seed finished"
+puts "#{Post.count} posts created"
+puts "#{Comment.count} comments created"
