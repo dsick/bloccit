f6dced7ce8ca54563d4281d8f2f51543c780874d
dsick <sick@davesick.net>
2016-04-21 13:11:48 -0700

    completed checkpoint 43

--- db/seeds.rb
+++ db/seeds.rb
@@ -30,13 +30,19 @@ topics = Topic.all
 # Create Posts
 50.times do
   # #1
-  Post.create!(
+  post = Post.create!(
     user:   users.sample,
     topic:  topics.sample,
     title:  RandomData.random_sentence,
     body:   RandomData.random_paragraph
   )
+  post.update_attribute(:created_at, rand(10.minutes .. 1.year).ago)
+  # #13
+  rand(1..5).times { post.votes.create!(value: [-1, 1].sample, user: users.sample) }
 end
+
+
+
 posts = Post.all
 
 # Create Comments
@@ -50,22 +56,23 @@ posts = Post.all
 end
 
 # Create an admin user
- admin = User.create!(
-   name:     'Admin User',
-   email:    'admin@example.com',
-   password: 'helloworld',
-   role:     'admin'
- )
- 
- # Create a member
- member = User.create!(
-   name:     'Member User',
-   email:    'member@example.com',
-   password: 'helloworld'
- )
+admin = User.create!(
+  name:     'Admin User',
+  email:    'admin@example.com',
+  password: 'helloworld',
+  role:     'admin'
+)
+
+# Create a member
+member = User.create!(
+  name:     'Member User',
+  email:    'member@example.com',
+  password: 'helloworld'
+)
 
 puts "Seed finished"
 puts "#{User.count} users created"
 puts "#{Topic.count} topics created"
 puts "#{Post.count} posts created"
 puts "#{Comment.count} comments created"
+puts "#{Vote.count} votes created"
