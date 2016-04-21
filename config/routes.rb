2d35d7eef4dc59847ca99fb419411fe96d8dd354
dsick <sick@davesick.net>
2016-04-21 16:22:09 -0700

    checkpoint 44 completed

--- config/routes.rb
+++ config/routes.rb
@@ -6,8 +6,8 @@ Rails.application.routes.draw do
   end
 
   resources :posts, only: [] do
-    # #5
     resources :comments, only: [:create, :destroy]
+    resources :favorites, only: [:create, :destroy]
 
     post '/up-vote' => 'votes#up_vote', as: :up_vote
     post '/down-vote' => 'votes#down_vote', as: :down_vote
