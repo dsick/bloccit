fd1ecc091187aebf6735b3b114aeca81c2a58d47
dsick <sick@davesick.net>
2016-04-25 10:45:32 -0700

    reverting topics contoller spec

--- spec/controllers/topics_controller_spec.rb
+++ spec/controllers/topics_controller_spec.rb
@@ -4,7 +4,6 @@ include SessionsHelper
 
 RSpec.describe TopicsController, type: :controller do
   let(:my_topic) { create(:topic) }
-  let(:my_private_topic) { create(:topic, public: false) }
 
   context "guest" do
     describe "GET index" do
@@ -13,14 +12,9 @@ RSpec.describe TopicsController, type: :controller do
         expect(response).to have_http_status(:success)
       end
 
-      it "does not include private topics in @topics" do
-         get :index
-         expect(assigns(:topics)).not_to include(my_private_topic)
-       end
-
       it "assigns Topic.all to topic" do
         get :index
-         expect(assigns(:topics)).to eq([my_topic, my_private_topic])
+        expect(assigns(:topics)).to eq([my_topic])
       end
     end
 
@@ -168,7 +162,7 @@ RSpec.describe TopicsController, type: :controller do
 
       it "assigns Topic.all to topic" do
         get :index
-        expect(assigns(:topics)).to eq([my_topic, my_private_topic])
+        expect(assigns(:topics)).to eq([my_topic])
       end
     end
 
