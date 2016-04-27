9db1d1c79c338d599fe52f114daee52c96a0510a
dsick <sick@davesick.net>
2016-04-27 12:17:08 -0700

    added a test case

--- spec/views/users/show.html.erb_spec.rb
+++ spec/views/users/show.html.erb_spec.rb
@@ -5,12 +5,14 @@ RSpec.describe "users/show.html.erb", type: :view do
   let(:my_user) { create(:user, email: "sick@davesick.net") }
   let(:my_topic) { create(:topic) }
   let(:my_post) { create(:post, title: "My awesome title", topic: my_topic, user: my_user) }
-  let(:my_comment) { create(:comment, post: my_post, user: my_user) }
+  let!(:my_comment) { create(:comment, post: my_post, user: my_user) }
 
   describe "users/show" do
     it "renders" do
-      render template: 'users/show', user: my_user, post: my_post
-      rendered.should contain("My awesome title")
+      assign :user, my_user
+      assign :favorites, []
+      render template: 'users/show'
+      expect(rendered).to include("My awesome title")
     end
   end
 end
